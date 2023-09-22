ef639258aabd0f7240c3102ed072a9fec2f1a25c
JohnyWander <j.myslinski.pl@gmail.com>
2023-09-22 14:14:34 +0200

    Dodaj pliki projektów.

---
+++ Mailer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Net;
+
+namespace LightNetMailer
+{
+    internal class Mailer
+    {
+
+        public string From;
+        private string FromName;
+        private string FromEmail;
+
+        public string To;
+        public string cc;
+        public string bcc;
+
+        public string ServerNPort
+        {
+            set
+            {
+                string[] splitted = value.Split(':');
+                this.ServerAdress = splitted[0];
+                this.Port = int.Parse(splitted[1]);
+            }
+        }
+             
+        private string ServerAdress;
+        private int Port;
+
+
+        public string Title;
+        public string PathToMessageFile;
+        public string charset;
+        public string PathToLogFile;
+        public string username;
+        public string password;
+        public List<string> attachmentsPaths = new List<string>();
+
+
+
+        SmtpClient smtpClient;
+        MailAddress fromMail;
+
+        MailAddressCollection toMails = new MailAddressCollection();
+        MailAddressCollection ccMails = new MailAddressCollection();
+        MailAddressCollection bccMails = new MailAddressCollection();
+
+        List<Attachment> attachments = new List<Attachment>();
+        
+        
+        MailMessage MailMessage;
+        public void PrepareSmtpClient()
+        {           
+            smtpClient = new SmtpClient(ServerAdress,Port);
+            smtpClient.UseDefaultCredentials = false;           
+            smtpClient.EnableSsl = true;
+            NetworkCredential creds = new NetworkCredential(this.username.Trim(), this.password.Trim());         
+            smtpClient.Credentials = creds;                   
+        }
+
+        public void PrepareFrom()
+        {            
+            string email = this.From.Substring(this.From.IndexOf('<'));
+            email = email.Trim('<').Trim('>');            
+            string name = this.From.Substring(0, this.From.IndexOf("<")).Trim();                   
+            this.fromMail = new MailAddress(email,name);
+        }
+
+        public void PrepareTo()
+        {
+            if (this.To.Contains(";"))
+            {
+                string[] addresses = this.To.Split(";");
+                foreach(string a in addresses)
+                {
+                    toMails.Add(new MailAddress(a));
+                }
+
+            }
+            else
+            {
+                toMails.Add(new MailAddress(this.To));
+            }
+        }
+
+        public void PrepareBCC()
+        {
+            if (this.bcc is not null)
+            {
+                if (this.bcc.Contains(";"))
+                {
+                    string[] addresses = this.bcc.Split(";");
+                    foreach (string a in addresses)
+                    {
+                        bccMails.Add(new MailAddress(a));
+                    }
+                }
+                else
+                {
+                    bccMails.Add(new MailAddress(this.bcc));
+                }
+            }
+        }
+
+        public void PrepareCC()
+        {
+            if (this.cc is not null)
+            {
+                if (this.cc.Contains(";"))
+                {
+                    string[] addresses = this.cc.Split(";");
+                    foreach (string a in addresses)
+                    {
+                        ccMails.Add(new MailAddress(a));
+                    }
+                }
+                else
+                {
+                    ccMails.Add(new MailAddress(this.cc));
+                }
+            }
+
+        }
+
+        public void PrepareAttachments()
+        {
+            foreach(string AttachmentsPath in this.attachmentsPaths)
+            {
+                this.attachments.Add(new Attachment(AttachmentsPath));              
+            }
+        }
+
+        public void PrepareMessage()
+        {
+            MailMessage = new MailMessage();
+
+            toMails.ToList().ForEach(mail => this.MailMessage.To.Add(mail));
+            ccMails.ToList().ForEach(mail => this.MailMessage.CC.Add(mail));
+            bccMails.ToList().ForEach(mail => this.MailMessage.Bcc.Add(mail));
+            attachments.ForEach(attachment => this.MailMessage.Attachments.Add(attachment));
+
+            this.MailMessage.From = this.fromMail;
+            this.MailMessage.Subject = this.Title;                     
+            this.MailMessage.Body = File.ReadAllText(this.PathToMessageFile.Split("=")[1]);
+            this.MailMessage.BodyEncoding = Encoding.UTF8;
+            this.MailMessage.IsBodyHtml = false;
+        }
+
+        public void SendMessage()
+        {
+
+              smtpClient.Send(MailMessage);
+           // Console.WriteLine(MailMessage.Bcc[0].Address.ToString());
+           
+        }
+
+
+        public Mailer()
+        {
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LightNetMailer
+{
+    internal class Program
+    {
+        static List<cmdarg> AvaiableCmdAargs = new List<cmdarg>()
+        {
+            new cmdarg("f","-f specify from name like \"name <email@mail.com>\"",(string from) => mailer.From = from),
+            new cmdarg("t","-t email@mail.com / email@mail.com;email1@mail.com - to mail address use \";\" for multiple recipents",(string to) => mailer.To = to),
+            new cmdarg("cc","-cc email@mail.com / email@mail.com;email1@email.com - carbon copy adress \";\" for multiple recipents",(string cc) => mailer.cc = cc),
+            new cmdarg("bcc","-bcc email@mail.com - mail@mail.com;email1@email.com - carbon copy adress \";\" for multiple recipents ",(string bcc) => mailer.bcc = bcc),
+            new cmdarg("s","-s server.com:222 - server and port",(string server) =>mailer.ServerNPort = server),
+            new cmdarg("u","-u Message title",(string title) => mailer.Title = title),
+            new cmdarg("a","-a path\\To\\Attachment - ads attachment to mail",(string Attachment) => mailer.attachmentsPaths.Add(Attachment)),
+            new cmdarg("o","-o path\\to\\message_file - adds message body from file",(string messageFile) => mailer.PathToMessageFile = messageFile ),
+            new cmdarg("l","-l path\\to\\logfile - sets path to log file and enables logging",(string logfile) => mailer.PathToLogFile = logfile),
+            new cmdarg("xu","-xu username - username to smtp server login", (string username) => mailer.username = username),
+            new cmdarg("xp","-xp password - password to smtp server login", (string password) => mailer.password = password),
+            new cmdarg("h","-h - displays help message",(string x) => DisplayHelpMessage())
+
+        };
+
+        static List<cmdarg> ActivatedCmdArgs = new List<cmdarg>();
+
+        static Mailer mailer = new Mailer();
+
+
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                ParseCommandLineArgs(args);
+                InvokeParamSetters();
+
+                mailer.PrepareSmtpClient();
+                mailer.PrepareFrom();
+                mailer.PrepareTo();
+                mailer.PrepareCC();
+                mailer.PrepareBCC();
+                mailer.PrepareAttachments();
+                mailer.PrepareMessage();
+                mailer.SendMessage();
+
+            }
+            Console.ReadLine();
+        }
+
+        static void ParseCommandLineArgs(string[] args)
+        {
+            StringBuilder fullargs = new StringBuilder();
+            fullargs.Append(" ");
+            foreach (string arg in args)
+            {
+              
+                fullargs.Append(arg + " ");
+            }
+
+            //Console.WriteLine(fullargs);
+            string fullargsstring = fullargs.ToString();
+            string[] splittedArgPairs = fullargsstring.Split(" -", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in splittedArgPairs)
+            {
+                string pName;
+                string value;
+                string[] split = s.Split(" ",2);
+                pName = split[0];
+                value = split[1];
+
+                
+                IEnumerable<cmdarg> pickedParam= null;
+              
+                pickedParam = AvaiableCmdAargs.Where(arg => arg.ArgName == pName);
+                
+
+                if (pickedParam.Count() == 1)
+                {
+                    cmdarg p = pickedParam.ToArray()[0];
+                    ActivatedCmdArgs.Add(new cmdarg(p.ArgName,p.ArgHelpMessage,value,p.ArgAction));
+
+                }
+            }
+            
+        }
+
+        static void InvokeParamSetters()
+        {
+            foreach(cmdarg p in ActivatedCmdArgs)
+            {
+                p.ArgAction.Invoke(p.ArgValue);
+            }
+        }
+        static void DisplayHelpMessage()
+        {
+            foreach(cmdarg c in AvaiableCmdAargs)
+            {
+                Console.WriteLine($"{c.ArgName}, {c.ArgHelpMessage}");
+            }
+        }
+
+
+
+
+    }
+}
